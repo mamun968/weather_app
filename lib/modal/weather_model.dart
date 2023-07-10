fe38f57bd17b9df5a5fd0206467c62d4cad73d0d
MD MAMUN HOSSAIN <110564652+mamun968@users.noreply.github.com>
2023-07-11 02:14:41 +0600

    final

--- lib/modal/weather_model.dart
+++ lib/modal/weather_model.dart
@@ -20,14 +20,15 @@ class Weather {
     this.description,
   });
 
-  Weather.fromJson(Map<String, dynamic> json) {
-    cityName = json["name"];
-    description = json["weather"][0]["description"];
-    temp = json["main"]["temp"] ;
-    wind = json["wind"]["speed"];
-    humidity = json["main"]["humidity"];
-    feelsLike = json["main"]["feels_like"] ;
-    pressure = json["main"]["pressure"];
-    icon = json["weather"][0]["icon"];
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+        cityName: json["name"],
+        description: json["weather"][0]["description"],
+        temp: json["main"]["temp"],
+        wind: json["wind"]["speed"],
+        humidity: json["main"]["humidity"],
+        feelsLike: json["main"]["feels_like"],
+        pressure: json["main"]["pressure"],
+        icon: json["weather"][0]["icon"]);
   }
 }
